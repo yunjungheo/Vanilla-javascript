926e634851b0d01a24363197cecb3c098c09602d
yunjungheo <ggomi7900@gmail.com>
2022-03-11 00:21:57 +0900

    randomness background

---
+++ js/quotes.js
@@ -0,0 +1,49 @@
+const quotes = [
+  {
+    quote: "It is better to fail in originality than to succeed in imitation.",
+    author: "Herman Melville",
+  },
+  {
+    quote: "Success is walking from failure to failure with no loss of enthusiasm",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "All progress takes place outside the comfort zone.",
+    author: "Michael John",
+  },
+  {
+    quote: "Success usually comes to those who are too busy to be looking for it.",
+    author: "Henry David Thoreau",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    author: "Albert Schweitzer",
+  },
+  {
+    quote: "Success seems to be connected with action. Successful people keep moving.",
+    author: "Conrad Hilton",
+  },
+  {
+    quote: "In order to succeed, we must first believe that we can.",
+    author: "Nikos Kazantzakis",
+  },
+  {
+    quote: "The only place where success comes before work is in the dictionary",
+    author: "Vidal Sassoon",
+  },
+  {
+    quote: "The past can hurt, but the way I see it, you can either run from it or learn from it",
+    author: "Lion King",
+  },
+];
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length )];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
